8a74bb423ffcff7b3601ac6101cb4ef81f1f363d
Holly Cummins <hcummins@redhat.com>
2023-06-20 14:18:41 +0200

    Migrate to Quarkus 3

--- bff/src/main/java/sample/house/ResidentResource.java
+++ bff/src/main/java/sample/house/ResidentResource.java
@@ -6,11 +6,11 @@ import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import javax.ws.rs.CookieParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.CookieParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import sample.resident.Resident;
 import sample.rooms.Bathroom;
